07d5d80a50e9a94520cd7e7b7724eee238977eb5
Jellyfish042 <1604466216@qq.com>
2025-01-26 15:16:49 +0800

    hf models

--- rwkv_mmlu_minimal.py
+++ rwkv_mmlu_minimal.py
@@ -13,31 +13,31 @@ torch.backends.cuda.matmul.allow_tf32 = True
 from torch.nn import functional as F
 from datasets import load_dataset, load_from_disk
 
-os.environ["RWKV_JIT_ON"] = "1"
-os.environ["RWKV_CUDA_ON"] = "1"
-
-from rwkv.model import RWKV
-from rwkv.utils import PIPELINE
-
 ########################################################################################################
 # MODEL
-MODEL_NAME = "RWKV-x060-World-7B-v2.1-20240507-ctx4096.pth"
+MODEL_NAME = "rwkv-x070-1b5-world-v3-80%trained-20250120-ctx4k"
+MODEL_TYPE = "rwkv7"  # rwkv6, rwkv7, hf
+# MODEL_NAME = "Qwen/Qwen2.5-0.5B"
+# MODEL_TYPE = "hf"
 
 print(f"Loading model - {MODEL_NAME}")
-model = RWKV(model=MODEL_NAME, strategy="cuda fp16", verbose=False)
-pipeline = PIPELINE(model, "rwkv_vocab_v20230424")
-########################################################################################################
-# MMLU DATASET
-# mmlu = load_dataset("cais/mmlu", 'all')
+if "rwkv" in MODEL_TYPE:
+    if MODEL_TYPE == "rwkv7":
+        os.environ["RWKV_V7_ON"] = "1"
+    os.environ["RWKV_JIT_ON"] = "1"
+    os.environ["RWKV_CUDA_ON"] = "1"
 
-# mmlu_test = mmlu['test']
-# mmlu_dev = mmlu['dev']
+    from rwkv.model import RWKV
+    from rwkv.utils import PIPELINE
 
-# mmlu_test.save_to_disk('mmlu_test_dataset')
-# mmlu_dev.save_to_disk('mmlu_dev_dataset')
+    model = RWKV(model=MODEL_NAME, strategy="cuda fp16")
+    pipeline = PIPELINE(model, "rwkv_vocab_v20230424")
+    tokenizer = pipeline.tokenizer
+elif MODEL_TYPE == "hf":
+    from transformers import AutoTokenizer, AutoModelForCausalLM
 
-mmlu_test = load_from_disk("mmlu_test_dataset")
-mmlu_dev = load_from_disk("mmlu_dev_dataset")
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, device_map="cuda:0").eval()
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 ########################################################################################################
 # PROMPT TEMPLATE
 # lm_eval baseline - 42.8%
@@ -51,8 +51,18 @@ mmlu_dev = load_from_disk("mmlu_dev_dataset")
 # Answer:'''
 
 # better format for RWKV - 46.7%
-TEMPLATE = """
-User: <Q>
+# TEMPLATE = """
+# User: <Q>
+# A. <|A|>
+# B. <|B|>
+# C. <|C|>
+# D. <|D|>
+
+# Assistant: The answer is"""
+
+# prompt template pro max - 47.9%
+TEMPLATE = """User: You are a very talented expert in <SUBJECT>. Answer this question:
+<Q>
 A. <|A|>
 B. <|B|>
 C. <|C|>
@@ -60,19 +70,20 @@ D. <|D|>
 
 Assistant: The answer is"""
 
-# prompt template pro max - 47.7%
-# TEMPLATE = '''User: You are a very talented expert in <SUBJECT>. Answer this question:
-# <Q>
-# A. <|A|>
-# B. <|B|>
-# C. <|C|>
-# D. <|D|>
-
-# Assistant: The answer is'''
-
 # choices
 CHOICES = [" A", " B", " C", " D"]
+########################################################################################################
+# MMLU DATASET
+# mmlu = load_dataset("cais/mmlu", 'all')
 
+# mmlu_test = mmlu['test']
+# mmlu_dev = mmlu['dev']
+
+# mmlu_test.save_to_disk('mmlu_test_dataset')
+# mmlu_dev.save_to_disk('mmlu_dev_dataset')
+
+mmlu_test = load_from_disk("mmlu_test_dataset")
+mmlu_dev = load_from_disk("mmlu_dev_dataset")
 ########################################################################################################
 # SET RANDOM SEED
 SHUFFLE = False
@@ -88,10 +99,14 @@ correct = 0
 total = 0
 pbar = tqdm(total=len(mmlu_test))
 
-choices_token = [pipeline.tokenizer.encode(x) for x in CHOICES]
-assert all([len(x) == 1 for x in choices_token]), "Choices are not single token, use rwkv_mmlu.py instead"
+if "rwkv" in MODEL_TYPE:
+    choices_token = [tokenizer.encode(x) for x in CHOICES]
+elif MODEL_TYPE == "hf":
+    choices_token = tokenizer(CHOICES)["input_ids"]
+assert all([len(x) == 1 for x in choices_token])
 choices_token = [x[0] for x in choices_token]
 
+score_by_subject = {}
 for idx, sample in enumerate(mmlu_test):
     question = sample["question"]
     choices = sample["choices"]
@@ -119,20 +134,30 @@ for idx, sample in enumerate(mmlu_test):
         print("-" * 100)
         format_example = all_prefix
 
-    # all_prefix_ids = pipeline.tokenizer.encode(all_prefix)
-    all_prefix_ids = [0] + pipeline.tokenizer.encode(all_prefix.strip())
+    if "rwkv" in MODEL_TYPE:
+        all_prefix_ids = [0] + tokenizer.encode(all_prefix.strip())  # RWKV padding
+        logits, _ = model.forward(all_prefix_ids, None, full_output=False)
+    elif MODEL_TYPE == "hf":
+        all_prefix_ids = tokenizer.encode(all_prefix.strip(), return_tensors="pt")
+        all_prefix_ids = all_prefix_ids.to(model.device)
+        logits = model.forward(all_prefix_ids).logits[0, -1, :]
 
-    logits, _ = model.forward(all_prefix_ids, None, full_output=False)
-    neg_log_prob = F.log_softmax(logits, dim=-1)
-    target_prob = neg_log_prob[choices_token]
+    log_prob = F.log_softmax(logits, dim=-1)
+    target_prob = log_prob[choices_token]
+    if subject not in score_by_subject:
+        score_by_subject[subject] = {"correct": 0, "total": 0}
     if torch.argmax(target_prob).item() == gt:
         correct += 1
+        score_by_subject[subject]["correct"] += 1
     total += 1
+    score_by_subject[subject]["total"] += 1
     pbar.set_description(f"Correct: {correct} - Total: {total} - Accuracy: {correct / total:.5f}")
     pbar.update(1)
 pbar.close()
 
 # Save results
+for subject in score_by_subject:
+    score_by_subject[subject]["accuracy"] = score_by_subject[subject]["correct"] / score_by_subject[subject]["total"]
 now = datetime.datetime.now()
 file_name = f'logs/mmlu_test_results_{now.strftime("%Y%m%d%H%M%S")}.json'
 with open(file_name, "w") as f:
@@ -146,6 +171,7 @@ with open(file_name, "w") as f:
             "example": format_example,
             "shuffle": SHUFFLE,
             "seed": SEED,
+            "score_by_subject": score_by_subject,
         },
         f,
         indent=4,
