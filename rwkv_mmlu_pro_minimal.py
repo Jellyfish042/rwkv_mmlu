eb5ad65f2774e2d55838c638deb9156f1738f64b
Jellyfish042 <1604466216@qq.com>
2025-02-13 21:39:32 +0800

    eval mmlu pro

---
+++ rwkv_mmlu_pro_minimal.py
@@ -0,0 +1,156 @@
+########################################################################################################
+# The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
+########################################################################################################
+import os, json, datetime, random
+from tqdm import tqdm
+import numpy as np
+import torch
+
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cuda.matmul.allow_tf32 = True
+
+from torch.nn import functional as F
+from datasets import load_dataset, load_from_disk
+
+########################################################################################################
+# MODEL
+MODEL_NAME = "RWKV-x070-World-0.4B-v2.9-20250107-ctx4096"
+MODEL_TYPE = "rwkv7"  # rwkv6, rwkv7, hf
+# MODEL_NAME = "Qwen/Qwen2.5-0.5B"
+# MODEL_TYPE = "hf"
+
+print(f"Loading model - {MODEL_NAME}")
+if "rwkv" in MODEL_TYPE:
+    if MODEL_TYPE == "rwkv7":
+        os.environ["RWKV_V7_ON"] = "1"
+    os.environ["RWKV_JIT_ON"] = "1"
+    os.environ["RWKV_CUDA_ON"] = "1"
+
+    from rwkv.model import RWKV
+    from rwkv.utils import PIPELINE
+
+    model = RWKV(model=MODEL_NAME, strategy="cuda fp16")
+    pipeline = PIPELINE(model, "rwkv_vocab_v20230424")
+    tokenizer = pipeline.tokenizer
+elif MODEL_TYPE == "hf":
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, device_map="cuda:0").eval()
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+########################################################################################################
+# PROMPT TEMPLATE - Exeeding choices will be ignored
+TEMPLATE = """User: You are a very talented expert in <|SUBJECT|>. Answer this question:
+<|QUESTION|>
+<|CHOICES|>
+
+Assistant: The answer is"""
+
+CHOICES_TEMPLATE = "<|INDEX|>. <|CHOICE|>\n"
+
+# choices
+CHOICES = [" A", " B", " C", " D", " E", " F", " G", " H", " I", " J"]  # maximum 10 choices in MMLU-Pro
+########################################################################################################
+# MMLU Pro DATASET
+# mmlu_pro = load_dataset("TIGER-Lab/MMLU-Pro")  # you need to login via huggingface-cli to download the dataset
+
+# mmlu_pro_test = mmlu_pro['test']
+# mmlu_pro_dev = mmlu_pro["validation"]
+
+# mmlu_pro_test.save_to_disk('mmlu_pro_test_dataset')
+# mmlu_pro_dev.save_to_disk('mmlu_pro_dev_dataset')
+
+mmlu_pro_test = load_from_disk("mmlu_pro_test_dataset")
+mmlu_pro_dev = load_from_disk("mmlu_pro_dev_dataset")
+########################################################################################################
+# SET RANDOM SEED
+SHUFFLE = False
+SEED = 42
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+########################################################################################################
+# RUN EVALUATION
+correct = 0
+total = 0
+pbar = tqdm(total=len(mmlu_pro_test))
+
+if "rwkv" in MODEL_TYPE:
+    choices_token = [tokenizer.encode(x) for x in CHOICES]
+elif MODEL_TYPE == "hf":
+    choices_token = tokenizer(CHOICES)["input_ids"]
+assert all([len(x) == 1 for x in choices_token])
+choices_token = [x[0] for x in choices_token]
+
+score_by_subject = {}
+for idx, sample in enumerate(mmlu_pro_test):
+    question = sample["question"]
+    choices = sample["options"]
+    subject = sample["category"]
+    gt = sample["answer_index"]
+
+    if SHUFFLE and not any(["Both" in x for x in choices]):  # exclude choices like "Both A and B"
+        original_gt_text = choices[gt]
+        np.random.shuffle(choices)
+        gt = choices.index(original_gt_text)
+
+    choices_str = "".join(
+        [
+            CHOICES_TEMPLATE.replace("<|INDEX|>", str(i)).replace("<|CHOICE|>", x)
+            for i, x in zip(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"], choices)
+        ]
+    )
+    all_prefix = TEMPLATE.replace("<|SUBJECT|>", subject).replace("<|QUESTION|>", question).replace("<|CHOICES|>", choices_str)
+
+    if idx == 0:
+        print(f"Format example:")
+        print("-" * 100)
+        print(all_prefix)
+        print("-" * 100)
+        format_example = all_prefix
+
+    if "rwkv" in MODEL_TYPE:
+        all_prefix_ids = [0] + tokenizer.encode(all_prefix.strip())  # RWKV padding
+        logits, _ = model.forward(all_prefix_ids, None, full_output=False)
+    elif MODEL_TYPE == "hf":
+        all_prefix_ids = tokenizer.encode(all_prefix.strip(), return_tensors="pt")
+        all_prefix_ids = all_prefix_ids.to(model.device)
+        logits = model.forward(all_prefix_ids).logits[0, -1, :]
+
+    log_prob = F.log_softmax(logits, dim=-1)
+    target_prob = log_prob[choices_token]
+    if subject not in score_by_subject:
+        score_by_subject[subject] = {"correct": 0, "total": 0}
+    if torch.argmax(target_prob).item() == gt:
+        correct += 1
+        score_by_subject[subject]["correct"] += 1
+    total += 1
+    score_by_subject[subject]["total"] += 1
+    pbar.set_description(f"Correct: {correct} - Total: {total} - Accuracy: {correct / total:.5f}")
+    pbar.update(1)
+pbar.close()
+
+# Save results
+for subject in score_by_subject:
+    score_by_subject[subject]["accuracy"] = score_by_subject[subject]["correct"] / score_by_subject[subject]["total"]
+now = datetime.datetime.now()
+file_name = f'logs/mmlu_test_results_{now.strftime("%Y%m%d%H%M%S")}.json'
+with open(file_name, "w") as f:
+    json.dump(
+        {
+            "model": MODEL_NAME,
+            "correct": correct,
+            "total": total,
+            "accuracy": correct / total,
+            "template": TEMPLATE,
+            "example": format_example,
+            "shuffle": SHUFFLE,
+            "seed": SEED,
+            "score_by_subject": score_by_subject,
+        },
+        f,
+        indent=4,
+    )
+print(f"Results saved to {file_name}")
